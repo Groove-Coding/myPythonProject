c80d4c4c1bed9abe76e1cc2b6b6762ddd72c778c
groovecoding7 <groovecoding@gmail.com>
2021-03-16 19:00:28 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def printxtimes(loopNum, name):
+    for y in range(loopNum):
+        label = "Hi {} number {}."
+        print(label.format(name, y))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    loopNum = 50
+    printxtimes(loopNum, 'PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
