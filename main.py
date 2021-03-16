276df9ecad4afb2bacdfb0b6e67ffdfe9b1b02ea
groovecoding7 <groovecoding@gmail.com>
2021-03-16 19:10:09 -0400

    added name variable

--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def printxtimes(loopNum, name):
+def printxtimes(loopNum, firstName, middleName, lastName):
     for y in range(loopNum):
-        label = "Hi {} number {}."
-        print(label.format(name, y))
+        label = "Hi {} {} {} number {}."
+        print(label.format(firstName, middleName, lastName, y))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     loopNum = 50
-    name = 'PyCharm'
-    printxtimes(loopNum, name)
+    firstName = '1'
+    middleName = '2'
+    lastName = '3'
+    printxtimes(loopNum, firstName, middleName, lastName)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
